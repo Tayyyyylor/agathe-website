89bf3d65334af13a02d5bfee5f17a52e2627a48a
Tayyyyylor <bryan.h.pro@gmail.com>
2025-03-04 15:05:23 +0100

    add last fixes

--- src/components/atoms/counterSlide/CounterSlide.tsx
+++ src/components/atoms/counterSlide/CounterSlide.tsx
@@ -9,24 +9,20 @@ interface CounterSlideProps {
     className?: string
     data: any
     index: number
-    setIndex: (index: number) => void
 }
 
 export default function CounterSlide({
     className,
     data,
     index,
-    setIndex,
 }: CounterSlideProps) {
     const containerRef = useRef<HTMLDivElement | null>(null)
     const currentNumberRef = useRef<HTMLDivElement | null>(null)
-    const hoverContainerRef = useRef<HTMLDivElement | null>(null)
     const numberOfPhotos = data.length
     const [currentOrder, setCurrentOrder] = useState(
         data[index]?.fields?.order || 0
     )
     const [firstLoad, setFirstLoad] = useState(true)
-    const [isHover, setIsHover] = useState(false)
 
     useEffect(() => {
         if (firstLoad) {
@@ -64,24 +60,6 @@ export default function CounterSlide({
     }, [index])
 
     useGSAP(() => {
-        if (!isHover || !hoverContainerRef.current) return
-
-        const items = Array.from(hoverContainerRef.current.children)
-        const tl = gsap.timeline()
-        tl.fromTo(
-            items,
-            { x: 50, opacity: 0 }, // Position initiale
-            {
-                x: 0,
-                opacity: 1,
-                duration: 0.5,
-                ease: 'power2.out',
-                stagger: 0.1, // Décalage entre les chiffres
-            }
-        )
-    }, [isHover])
-
-    useGSAP(() => {
         if (!containerRef.current) return
 
         gsap.fromTo(
@@ -95,39 +73,10 @@ export default function CounterSlide({
         return null
     }
 
-    const handleHoverEnter = () => {
-        setIsHover(true)
-    }
-
-    const handleHoverLeave = () => {
-        setIsHover(false)
-    }
-
-    const handleNumberClick = (i: number) => {
-        setIndex(i)
-    }
     console.log('currentOrder', currentOrder)
     return (
-        <div
-            className={`${styles.counter} ${className}`}
-            ref={containerRef}
-            onMouseEnter={handleHoverEnter}
-            onMouseLeave={handleHoverLeave}
-        >
-            {isHover && (
-                <div className={styles.counterHover} ref={hoverContainerRef}>
-                    {data.map((_: null, i: number) => (
-                        <div
-                            key={i}
-                            className={styles.counterHoverItem}
-                            onClick={() => handleNumberClick(i)}
-                        >
-                            {i + 1}.
-                        </div>
-                    ))}
-                </div>
-            )}
-            {!isHover && <div ref={currentNumberRef}>{currentOrder}</div>}
+        <div className={`${styles.counter} ${className}`} ref={containerRef}>
+            <div ref={currentNumberRef}>{currentOrder}</div>
             <span>/{numberOfPhotos}</span>
         </div>
     )
--- src/components/templates/contact/Contact.tsx
+++ src/components/templates/contact/Contact.tsx
@@ -1,16 +1,41 @@
+'use client'
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useRef } from 'react'
+import gsap from 'gsap'
 import Link from 'next/link'
-import React from 'react'
+import { useGSAP } from '@gsap/react'
 import styles from './Contact.module.scss'
 
 export default function Contact({ data }: any) {
+    const containerRef = useRef<HTMLDivElement>(null)
     console.log('data', data)
+
+    useGSAP(() => {
+        if (!containerRef.current) return
+        const links = containerRef?.current?.querySelectorAll(`.${styles.link}`)
+        if (links) {
+            gsap.fromTo(
+                links,
+                { y: 100, opacity: 0 },
+                {
+                    y: 0,
+                    opacity: 1,
+                    duration: 0.5,
+                    ease: 'back.out(1.7)',
+                    stagger: 0.1,
+                }
+            )
+        }
+    }, [data])
+
     return (
-        <main className={styles.contact}>
-            <Link target="_blank" href={data.instagram}>
+        <main className={styles.contact} ref={containerRef}>
+            <Link target="_blank" href={data.instagram} className={styles.link}>
                 Instagram
             </Link>
-            <Link href={`mailto:${data.mail}`}>{data.mail}</Link>
+            <Link href={`mailto:${data.mail}`} className={styles.link}>
+                {data.mail}
+            </Link>
         </main>
     )
 }
